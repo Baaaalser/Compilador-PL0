fc2ffe673399194e544f18b7d2a3f1fcb1db4cf0
Walter Florentin <43592336+Baaaalser@users.noreply.github.com>
2024-03-08 21:58:03 -0300

    make sintactico dll

--- make-sintactico.bat
+++ make-sintactico.bat
@@ -1,9 +1,9 @@
 @echo off
-if exist analizadorsintactico.obj del analizadorsintactico.obj
-if exist analizadorsintactico.dll del analizadorsintactico.dll
+if exist analizador-sintactico.obj del analizador-sintactico.obj
+if exist analizador-sintactico.dll del analizador-sintactico.dll
 \masm32\bin\ml /c /coff analizadorsintactico.asm
-\masm32\bin\Link /SUBSYSTEM:WINDOWS /DLL /DEF:analizadorsintactico.def analizadorsintactico.obj 
-del analizadorsintactico.obj
-del analizadorsintactico.exp
-dir analizadorsintactico.*
+\masm32\bin\Link /SUBSYSTEM:WINDOWS /DLL /DEF:analizador-sintactico.def analizador-sintactico.obj 
+del analizador-sintactico.obj
+del analizador-sintactico.exp
+dir analizador-sintactico.*
 pause
